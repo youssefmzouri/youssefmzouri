dc6538ff83a67373f38ddc2fac6b0b93e4b90bb5
Youssef <youssefmzouri@gmail.com>
2026-02-01 13:26:28 +0100

    feat: add SVG components for OpenURL and DropDown

---
+++ src/components/svg/DropDown.jsx
@@ -0,0 +1,17 @@
+const SVGComponent = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width={20}
+    height={20}
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth={2}
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    {...props}
+  >
+    <polyline points="6 9 12 15 18 9" />
+  </svg>
+);
+export default SVGComponent;
